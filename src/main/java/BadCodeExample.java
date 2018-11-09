e58c356af3559b7e785b26323ecde10f17ac62a1
Viktor Bielov <vikbelov@ukr.net>
2018-11-10 00:00:32 +0200

    Initial commit. Lesson 2

---
+++ src/main/java/BadCodeExample.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class BadCodeExample {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("https://www.google.com");
+
+        WebElement searchField = webDriver.findElement(By.id("lst-ib"));
+        searchField.sendKeys("Selenium");
+        searchField.sendKeys(Keys.ENTER);
+
+        List<WebElement> resultsList = webDriver.findElements(By.xpath("//div[@class='srg']/div[@class='g']"));
+        System.out.println(resultsList.size());
+    }
+}
