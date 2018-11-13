a5ce76fcfdf1fbd69cda23683624ff359cde817b
Viktor Bielov <vikbelov@ukr.net>
2018-11-13 22:07:18 +0200

    Lesson 3

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    @Test
+    public void negativeLoginTest(){
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("https://www.linkedin.com");
+
+        WebElement emailField = webDriver.findElement(By.xpath("//*[@id='login-email']"));
+        WebElement passwordField = webDriver.findElement(By.xpath("//*[@id='login-password']"));
+        WebElement signInButton = webDriver.findElement(By.xpath("//*[@id='login-submit']"));
+
+        emailField.sendKeys("a@b.c");
+        passwordField.sendKeys("");
+        signInButton.click();
+
+        //Verify that page Title is "LinkedIn: Log In or Sign Up"
+        Assert.assertEquals(webDriver.getTitle(), "LinkedIn: Log In or Sign Up");
+    }
+}
