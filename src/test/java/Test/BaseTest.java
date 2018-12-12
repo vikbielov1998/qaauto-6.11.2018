f66134bc9c82a5b517051faa816b848447a952cd
Viktor Bielov <vikbelov@ukr.net>
2018-12-12 21:43:15 +0200

    Lesson 12

--- src/test/java/Test/BaseTest.java
+++ src/test/java/Test/BaseTest.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 /**Parent class for all tests
  *
@@ -16,13 +18,27 @@ public class BaseTest {
 
     protected LoginPage loginPage;
 
+    @Parameters("browserName")
     /**Method executes before each test
      *
      */
     @BeforeMethod
-    public void beforeMethod() {
-        WebDriverManager.firefoxdriver().setup();
-        webDriver = new FirefoxDriver();
+    public void beforeMethod(@Optional("chrome") String browserName) {
+        if (browserName.toLowerCase().equals("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            webDriver = new FirefoxDriver();
+        }
+        if (browserName.toLowerCase().equals("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            webDriver = new ChromeDriver();
+        } else {
+            try {
+                throw new Exception("browserName " + browserName + " is not supported.");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
         webDriver.get("https://www.linkedin.com");
         loginPage = new LoginPage(webDriver);
     }
